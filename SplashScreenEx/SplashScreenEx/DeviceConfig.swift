975f7122d74c157a29544535d2987ccf037b27ff
Minho Kim <nobility7192@gmail.com>
2019-05-12 00:18:31 +0900

    Update SplashScreen Example

--- SplashScreenEx/SplashScreenEx/DeviceConfig.swift
+++ SplashScreenEx/SplashScreenEx/DeviceConfig.swift
@@ -7,35 +7,41 @@
 //
 
 import Foundation
+import SystemConfiguration
+
 class DeviceManager {
     static let shared: DeviceManager = DeviceManager()
     
-    var networkStuta: Bool {
+    var networkStatus: Bool {
         get {
-            var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
-            zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-            zeroAddress.sin_family = sa_family_t(AF_INET)
-            
-            let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-                $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
-                    SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
-                }
-            }
-            
-            var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
-            if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
-                return false
-            }
-            
-            // Working for Cellular and WIFI
-            let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
-            let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-            let ret = (isReachable && !needsConnection)
-            return ret
+            return checkDeviceNetworkStatus()
         }
     }
     
     private init() {
     }
     
+    private func checkDeviceNetworkStatus() -> Bool {
+        print("Check to Device Natwork Status....")
+        var zeroAddress = sockaddr_in(sin_len: 0, sin_family: 0, sin_port: 0, sin_addr: in_addr(s_addr: 0), sin_zero: (0, 0, 0, 0, 0, 0, 0, 0))
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags: SCNetworkReachabilityFlags = SCNetworkReachabilityFlags(rawValue: 0)
+        if SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) == false {
+            return false
+        }
+        
+        // Working for Cellular and WIFI
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let ret = (isReachable && !needsConnection)
+        return ret
+    }
 }
--- SplashScreenEx/SplashScreenEx/SplashView/ViewController.swift
+++ SplashScreenEx/SplashScreenEx/SplashView/ViewController.swift
@@ -16,15 +16,21 @@ class ViewController: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        if(DeviceManager.shared.networkStuta) {
+       checkDeviceNetworkStatus()
+    }
+    
+    func checkDeviceNetworkStatus() {
+        if(DeviceManager.shared.networkStatus) {
+            let firstVC = UIStoryboard(name: "First", bundle: nil).instantiateViewController(withIdentifier: "FirstViewController")
+            present(firstVC, animated: true, completion: nil)
             
         } else {
-            
-            let alert: UIAlertController
+            let alert: UIAlertController = UIAlertController(title: "네트워크 상태 확인", message: "네트워크가 불안정 합니다.", preferredStyle: .alert)
             let action: UIAlertAction = UIAlertAction(title: "다시 시도", style: .default, handler: { (ACTION) in
-                
+                self.checkDeviceNetworkStatus()
             })
-            
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
         }
     }
 }
